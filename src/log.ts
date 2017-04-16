82abc34e86c612e58d9899b898537fd575a23d63
andimarek <andimarek@fastmail.fm>
2017-04-16 11:09:57 +0200

    add chalk

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,8 @@
+import {red} from 'chalk';
 export function log(message: string) {
   console.log(message);
 }
 
 export function error(message: string) {
-  console.error(message);
+  console.log(red(message));
 }
