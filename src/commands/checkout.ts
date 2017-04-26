15712935f867c1d1d32a4543a4fa7859933e304e
Andreas Marek <andimarek@fastmail.fm>
2017-04-26 11:53:38 +0200

    async checkout

--- src/commands/checkout.ts
+++ src/commands/checkout.ts
@@ -1,32 +1,49 @@
 import { Repository, Config, getRepos, getConfig } from '../domain';
 import { spawn } from 'child_process';
-import {noop} from 'lodash';
-import {gitCloneInWorkspace} from '../git';
-import {log} from '../log';
-import {ensureDirExists} from '../util';
+import { noop } from 'lodash';
+import { gitCloneInWorkspace } from '../git';
+import { log } from '../log';
+import { ensureDirExists } from '../util';
+import { Command } from '../inputreader';
+import { mapLimit } from 'async';
 
-import {Command} from '../inputreader';
 const command: Command = {
   name: 'checkout',
-  arguments: [ 
-    {name: "dir"}
+  arguments: [
+    { name: "dir" }
   ],
   execute(args: string[]): Promise<void> {
     return checkout(args[0]);
   }
 };
 
+const maxParallel = 5;
+
 export default command;
 
-async function checkout(targetDir:string): Promise<void> {
-  const repos = getRepos(); 
+function checkout(targetDir: string): Promise<void> {
+  const repos = getRepos();
   const config = getConfig();
   const promises: Promise<any>[] = [];
-  await ensureDirExists(targetDir);
-  log.debug(`checking out all repos into: ${targetDir}`);
-  for (const repo of repos) {
-    log.debug(`checking out ${repo.url}`);
-    promises.push(gitCloneInWorkspace(repo.url, targetDir));
-  }
-  return Promise.all(promises).then(noop);
+  return ensureDirExists(targetDir).then(() => {
+    log.debug(`checking out all repos into: ${targetDir}`);
+    return new Promise<void>((resolve, reject) => {
+      mapLimit(repos, 5, async (repo, callback) => {
+        log.debug(`checking out ${repo.url}`);
+        try {
+          await gitCloneInWorkspace(repo.url, targetDir);
+          callback();
+        } catch (error) {
+          callback(error);
+        }
+      }, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          log.success(`checkout finished`);
+          resolve();
+        }
+      });
+    });
+  });
 }
