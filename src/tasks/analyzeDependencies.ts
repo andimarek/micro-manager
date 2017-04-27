c470584391b18805f8109c5b8d5a6ebf8f0d6c3a
Andreas Marek <andimarek@fastmail.fm>
2017-04-27 15:12:13 +0200

    improve output

--- src/tasks/analyzeDependencies.ts
+++ src/tasks/analyzeDependencies.ts
@@ -1,9 +1,10 @@
 import { getData, getRepositoryById, Project, PROJECT_TYPE_GRADLE, GradleComplexType } from '../domain';
-import { identity, pickBy, uniqBy, mapValues, sortBy, reduce, map, filter, forEach } from 'lodash';
+import { groupBy, identity, pickBy, uniqBy, mapValues, sortBy, reduce, map, filter, forEach } from 'lodash';
 import { log } from '../log';
 import { checkoutAllProjects } from '../checkout';
 import { getDependencies, Configuration } from '../gradle';
 import { firstElement, addToArray, mapLimit } from '../util';
+import {red, blue} from 'chalk';
 
 interface ProjectAndDependencies {
   project: Project;
@@ -35,13 +36,13 @@ export function findDifferentVersions(): Promise<any> {
 
 function checkVersion(dependencies: ProjectAndDependencies[]) {
   const relevantDependencies = getRuntimeDependencies(dependencies);
-  type VersionsByArtifact = { [name: string]: { project: Project, version: string, groupId: string }[] };
+  type VersionsByArtifact = { [name: string]: { project: Project, version: string, artifactId: string, groupId: string }[] };
   const versionByArtifact: VersionsByArtifact = reduce(relevantDependencies, (acc, dependency) => {
     forEach(dependency.configurations, (config) => {
       forEach(config.dependencies, (artifact) => {
         const artifactName = artifact.groupId + ':' + artifact.artifactId;
         acc[artifactName] = (acc[artifactName] || []);
-        acc[artifactName].push({ project: dependency.project, version: artifact.version, groupId: artifact.groupId });
+        acc[artifactName].push({ project: dependency.project, artifactId: artifact.artifactId, version: artifact.version, groupId: artifact.groupId });
       });
     });
     return acc;
@@ -56,15 +57,21 @@ function checkVersion(dependencies: ProjectAndDependencies[]) {
     return {
       name,
       groupId: infos[0].groupId,
+      artifactId: infos[0].artifactId,
       infos
     }
   });
 
   const sorted = sortBy(array, ({groupId}) => groupId);
-  forEach(sorted, ({name, infos}) => {
-    const versions = map(infos, (info) => info.version);
-    const projects = map(infos, (info) => info.project.name);
-    log(`${name} is used in different versions: `, versions, ' in this projects: ', projects);
+  const byGroupId = groupBy(array, ({groupId}) => groupId);
+  forEach(byGroupId, (withSameGroupId, groupId) => {
+    log(red(groupId + ':'));
+    forEach(withSameGroupId, ({name, infos, artifactId}) => {
+      const versions = map(infos, (info) => info.version);
+      const projects = map(infos, (info) => info.project.name);
+      log(`${artifactId} is used in different versions: `, versions, ' in these projects: ', projects);
+    });
+    log(``);
   });
 }
 
